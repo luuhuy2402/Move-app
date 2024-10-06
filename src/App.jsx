e323a2f9c4b9ad82f1dee9aeee5666c347829805
Luu Huy Hieu <xeoxeo1895@gmail.com>
2024-10-06 09:30:28 +0700

    Call api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import MovieList from "./components/MovieList";
 
 function App() {
+    const [movie, setMovie] = useState([]);
+    const [movieRate, setMovieRate] = useState([]);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+            };
+            const url1 =
+                "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+            const url2 =
+                "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+
+            const [res1, res2] = await Promise.all([
+                fetch(url1, options),
+                fetch(url2, options),
+            ]);
+
+            const data1 = await res1.json();
+            const data2 = await res2.json();
+            setMovie(data1.results);
+            setMovieRate(data2.results);
+        };
+        fetchMovie();
+    }, []);
     return (
         <>
-            <Header/>
-            <Banner/>
+            <div className="bg-black pb-10">
+                <Header />
+                <Banner />
+                <MovieList title="Phim hot" data={movie.slice(0, 5)} />
+                <MovieList title="Phim đề cử" data={movieRate.slice(0, 5)} />
+            </div>
         </>
     );
 }
